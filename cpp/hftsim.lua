04f9ad202962803a697d3cede0caefc6b93e422b
Darshan Koirala <theanimatorspal@gmail.com>
2025-04-13 21:12:58 +0545

    ❌ Point Graph Text not rendering Problem

--- cpp/hftsim.lua
+++ cpp/hftsim.lua
@@ -1,34 +1,38 @@
 require "bundle"
 function main()
           require "hftsim"
-          local hft         = Engine.GameFramework({
+          local glerp, glerp_2f,
+          glerp_3f, glerp_4f,
+          glerp_mat4f = Jmath.GetLerps()
+
+          local hft   = Engine.GameFramework({
                     validation = true,
                     bc = vec4(0, 0, 0, 1)
           })
+          local data  = {
+                    mat4(vec4(2, 3, 0, 0), vec4(1, 0, 0, 1), vec4(0), vec4(0)),
+                    mat4(vec4(1, 3, 0, 0), vec4(0, 1, 0, 1), vec4(0), vec4(0)),
+                    mat4(vec4(2, 2, 0, 0), vec4(0, 0, 1, 1), vec4(0), vec4(0)), }
           --          hft.scale = hft.wr.
           -- local text   = hft.wr.CreateTextLabel(vec3(100, 100, 50), vec3(1), hft.nf, "FUCK", vec4(1))
-          local debug_image = hft.I(vec3(100, 100, 1), vec3(500, 500, 1))
-          local scale       = hft.wr.CreateScale2D(
-                    vec3(100, 100, 1),
-                    vec3(500, 500, 1),
-                    hft.sf,
-                    vec3(15, 15, 5),
-                    vec3(0, 0, 0),
-                    vec3(4, 4, 4),
-                    "%.2f",
-                    "%.2f",
-                    vec4(1, 0, 0, 1)
-          )
-          hft.Update        = function()
+          local graph = hft.PointGraph(vec3(100, 100, 1), vec3(500, 500, 1))
+          hft.wr.AnimationGeneralPush(function(inT)
+                    local vec = glerp_3f(vec3(2), vec3(10), inT)
+                    graph.Update(vec3(100, 100, 1), vec3(500, 500, 1), data, {
+                              from = vec3(0),
+                              to = vec
+                    })
+          end, 0.1)
+          hft.Update   = function()
+                    -- print("MAJOR Update")
                     hft.wr:Update()
           end
 
-          hft.Dispatch      = function()
+          hft.Dispatch = function()
                     hft.wr:Dispatch()
-                    debug_image.DrawDebugLines()
           end
 
-          hft.Draw          = function()
+          hft.Draw     = function()
                     hft.wr:Draw()
           end
 
