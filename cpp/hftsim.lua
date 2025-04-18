cff5f123208df7bdcaf9e12359941d9e4323ddbf
Darshan Koirala <theanimatorspal@gmail.com>
2025-04-18 20:21:26 +0545

    ✅ Base UI Layout

--- cpp/hftsim.lua
+++ cpp/hftsim.lua
@@ -1,60 +1,91 @@
 require "bundle"
 function main()
-          require "hftsim"
+    require "hftsim"
+    Jkr.GetLayoutsAsVH()
+    local glerp, glerp_2f, glerp_3f, glerp_4f, glerp_mat4f = Jmath.GetLerps()
 
-          local market = hft.market()
-          Engine.log(market)
 
-          local glerp, glerp_2f,
-          glerp_3f, glerp_4f,
-          glerp_mat4f  = Jmath.GetLerps()
+    local app             = Engine.GameFramework({
+        validation = true,
+        bc = vec4(0, 0, 0, 1),
+        colors = {
+            b = vec4(0, 0.9, 0.9, 0.5),
+            t = vec4(0, 0, 0, 1),
+            tb = vec4(0, 0.9, 0.9, 1)
+        },
+        wd = vec2(1500, 800),
+        fd = vec2(1500, 800)
+    })
 
-          local hft    = Engine.GameFramework({
-                    validation = false,
-                    bc = vec4(0, 0, 0, 1)
-          })
-          local data   = {
-                    mat4(vec4(2, 3, 0, 0), vec4(1, 0, 0, 1), vec4(0), vec4(0)),
-                    mat4(vec4(1, 3, 0, 0), vec4(0, 1, 0, 1), vec4(0), vec4(0)),
-                    mat4(vec4(2, 2, 0, 0), vec4(0, 0, 1, 1), vec4(0), vec4(0)), }
-          --          hft.scale = hft.wr.
+    local c               = app.colors
 
-          local graph  =
-              hft.Graph()
-              .Config()
-              .Scale()
-              .Points(data)
+    local Window          = function(title, c1, c2)
+        return app.wr.CreateWindowScissor(
+            vec3(100, 100, 1),
+            vec3(100, 100, 1),
+            app.nf,
+            title, c1, c2, c2 * 0.5, nil, true)
+    end
 
-          -- hft.wr.AnimationGeneralPush(function(inT)
-          --           local vec = glerp_3f(vec3(2), vec3(10), inT)
-          --           graph.Config {
-          --                     from = vec3(0),
-          --                     to = vec
-          --           }
-          --           graph.Points(data)
-          -- end, 0.1)
+    local market_window   = Window("Market", c.t, vec4(1, 0.5, 0.5, 1))
+    local controls_window = Window("Controls", c.t, vec4(0.5, 1, 0.5, 1))
+    local data_window     = Window("Data", c.t, vec4(0.5, 0.5, 1, 1))
 
-          -- hft.wr.AnimationGeneralPush(function(inT)
-          --           local vec = glerp_3f(vec3(2), vec3(10), inT)
-          --           graph.Config {
-          --                     from = vec3(0),
-          --                     to = vec
-          --           }
-          --           graph.Scale()
-          -- end, 0.1)
+    local data            = {
+        mat4(vec4(2, 3, 0, 0), vec4(1, 0, 0, 1), vec4(0), vec4(0)),
+        mat4(vec4(1, 3, 0, 0), vec4(0, 1, 0, 1), vec4(0), vec4(0)),
+        mat4(vec4(2, 2, 0, 0), vec4(0, 0, 1, 1), vec4(0), vec4(0)), }
 
-          hft.Update   = function()
-                    -- print("MAJOR Update")
-                    hft.wr:Update()
-          end
+    --window.SetCentralComponent(graph)
+    -- window:Update(vec3(100, 100, 50), vec3(100, 100, 1))
 
-          hft.Dispatch = function()
-                    hft.wr:Dispatch()
-          end
+    local B               = app.B
+    local E               = B { e = true }
 
-          hft.Draw     = function()
-                    hft.wr:Draw()
-          end
+    local graph           = app.Graph()
+    local graph_padding   = app.Pad(graph)
 
-          hft.loop()
+    app.Graph()
+    market_window.SetCentralComponent(graph_padding)
+
+    local layout = H(
+        {
+            V(
+                {
+                    market_window,
+                    E,
+                    controls_window,
+                },
+                { 0.7, 0.001, 0.3 }
+            ),
+            E,
+            data_window
+        }, { 0.7, 0.001, 0.3 })
+
+    layout:Update(vec3(0, 0, app.bd), vec3(app.fd.x, app.fd.y, 1))
+
+    graph.Config().Scale().Points(data)
+
+    -- app.wr.AnimationGeneralPush(function(inT)
+    --     local vec = glerp_3f(vec3(2), vec3(10), inT)
+    --     graph.Config {
+    --         from = vec3(0),
+    --         to = vec
+    --     }.Clear().Points(data).Scale()
+    -- end, 0.1)
+
+    app.Update   = function()
+        -- print("MAJOR Update")
+        app.wr:Update()
+    end
+
+    app.Dispatch = function()
+        app.wr:Dispatch()
+    end
+
+    app.Draw     = function()
+        app.wr:Draw()
+    end
+
+    app.loop()
 end
